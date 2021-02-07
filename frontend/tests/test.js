ecf592b63835a358c7fb70e8186e1d350195b994
Jonathan Hacker <github@jhacker.de>
2021-02-07 21:47:46 +0100

    separate manage.py

--- frontend/tests/test.js
+++ frontend/tests/test.js
@@ -1,7 +1,7 @@
 child = require('child_process');
 
 test('check', () => {
-	let p = child.spawnSync('python3', ['-m', 'django', 'version']);
+	let p = child.spawnSync('python3', ['../manage.py']);
 	console.log(p.stdout.toString('utf8'));
 	if (p.error) {
 		throw p.error;
