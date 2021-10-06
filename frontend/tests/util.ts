df247dc8a50d944b28e22ea9e19db09314cfd694
Jonathan Hacker <github@jhacker.de>
2021-10-06 18:17:44 +0200

    extend env

--- frontend/tests/util.ts
+++ frontend/tests/util.ts
@@ -3,10 +3,11 @@ import * as fs from 'fs';
 
 export function renderTemplate(template, options?) {
   options = JSON.stringify(options) || '';
+  let env = process.env;
+  env["DEBUG"] = "1";
   const p = child.spawnSync('python3', ['../manage.py', 'rendertemplate', template, 'head.html', 'body.html', options], {
-    env: {
-      DEBUG: "1",
-    }});
+    env: env,
+  });
   if (p.error) {
     throw p.error;
   }
