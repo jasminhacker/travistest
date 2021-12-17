72d5e3b05b2338b1df4a8e17e4fe74663c5a1b27
Jonathan Hacker <github@jhacker.de>
2021-12-17 21:25:46 +0100

    testpypi

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("raveberry/VERSION") as f:
+with open("backend/VERSION") as f:
     version = f.read().strip()
 
 with open("README.md") as f:
@@ -18,21 +18,21 @@ with open("backend/requirements/screenvis.txt") as f:
     screenvis_packages = f.read().splitlines()
 
 setuptools.setup(
-    name="raveberry",
-    version=version,
+    name="raveberry_test",
+    version="version",
     author="Jonathan Hacker",
     author_email="raveberry@jhacker.de",
     description="A multi-user music server with a focus on participation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/raveberry/raveberry",
+    url="https://github.com/raveberry_test/raveberry",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Framework :: Django",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Programming Language :: Python :: 3",
     ],
-    packages=["raveberry"],
+    packages=["raveberry_test"],
     include_package_data=True,
     python_requires=">=3.8",
     extras_require={
